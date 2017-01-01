ede06af2c151c857959b5fd4e68736933dc662b6
Hemant Pema <hemant@pema.co.za>
2017-01-01 21:38:37 +0200

    added doctypes

---
+++ access_control/access_control/doctype/residential_unit/test_residential_unit.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Hemant and Karan and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Residential Unit')
+
+class TestResidentialUnit(unittest.TestCase):
+	pass

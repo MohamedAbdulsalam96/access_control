71fbf4b76116e3a449fd708aeb6cc533f67ab0e0
Hemant <hemant@pema.co.za>
2017-05-06 18:05:54 +0200

    Update unit api

--- access_control/access_control/doctype/residential_unit/residential_unit.py
+++ access_control/access_control/doctype/residential_unit/residential_unit.py
@@ -21,7 +21,7 @@ def unit(pin, unit_number):
 		if exists:
 			residents = frappe.db.sql("""select name, contact_type, contact, show_as from `tabResident`
 					where parent=%(name)s
-					order by idx desc""", {"name": exists}, as_dict=True)
+					order by idx""", {"name": exists}, as_dict=True)
 			#unit = frappe.get_doc('Residential Unit', exists)
 
 			return residents#residents #unit.residents
